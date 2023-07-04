2692fc8daaa5ba414e335bc8c2a13c53d325701b
Athanasios Anastasiou <athanastasiou@gmail.com>
2023-07-04 16:06:57 +0000

    Modified the test to include all existing elements

--- examples/test.py
+++ examples/test.py
@@ -7,10 +7,11 @@ if __name__ == "__main__":
     page_name = "Sample page"
     # Create a very simple HTML page
     the_page = HTMLPage(HTMLBody([
-                                  HTMLPreProcMarkdownDiv("# Heading\n\nWelcome to this page. Its name is {{page_name}}\n\nAnd here it is \n\n{{my_plot}}\n\n# Other heading \n\n And then more text", 
-                                      {"page_name":page_name,
-                                       #"my_plot":HTMLBokehLinePlot(x=[0,1,2,3], y=[0,1,2,3])})
-                                       "my_plot":HTMLBokehBarPlot(x=["A", "B", "C", "D"][::-1], y=[0,1,2,3][::-1],)})
+                                  HTMLPreProcMarkdownDiv("# Heading\n\nWelcome to this page. Its name is {{page_name}}\n\n## Some subheading\n\n{{P1}}\n\n## Some other subheading\n\n{{P2}}\n", 
+                                      {
+                                       "page_name":page_name,
+                                       "P1":HTMLBokehLinePlot(x=[0,1,2,3], y=[0,1,2,3]),
+                                       "P2":HTMLBokehBarPlot(x=["A", "B", "C"], y=[10,20,10])})
                                  ],["basic_style.css"]), 
                         [
                          HTMLMeta({"charset":"utf-8"}), 
